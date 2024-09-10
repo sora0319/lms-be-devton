c69622019c6a7b42b6ae227315f6b89696f67ab8
gomar <ju79318@naver.com>
2024-09-11 01:41:55 +0900

    mod : BoardRepository interface 로 수정

--- src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
@@ -1,4 +1,9 @@
 package com.example.ahimmoyakbackend.board.repository;
 
-public class BoardRepository {
+import com.example.ahimmoyakbackend.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Long> {
 }
