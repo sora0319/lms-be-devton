a1d925826ce2e361565d767f16a3360d63185ee3
gomar <ju79318@naver.com>
2024-09-11 01:43:39 +0900

    mod : Board 생성 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/service/BoardService.java
+++ src/main/java/com/example/ahimmoyakbackend/board/service/BoardService.java
@@ -1,4 +1,28 @@
 package com.example.ahimmoyakbackend.board.service;
 
+import com.example.ahimmoyakbackend.auth.repository.UserRepository;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateRequestDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateResponseDTO;
+import com.example.ahimmoyakbackend.board.entity.Board;
+import com.example.ahimmoyakbackend.board.entity.Type;
+import com.example.ahimmoyakbackend.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    public BoardCreateResponseDTO create(BoardCreateRequestDTO requestDTO, Type boardType) {
+        Board board = Board.builder()
+                .title(requestDTO.getTitle())
+                .content(requestDTO.getContent())
+                .boardType(boardType)
+                .build();
+        boardRepository.save(board);
+        return BoardCreateResponseDTO.builder().msg("게시물 작성 완료").build();
+    }
 }
