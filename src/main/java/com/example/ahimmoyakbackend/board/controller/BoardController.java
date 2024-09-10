c69b9624892b04dcb1bd9acb53fa74f37328d7a0
gomar <ju79318@naver.com>
2024-09-11 01:32:44 +0900

    feat : board 생성 기능 구현

--- src/main/java/com/example/ahimmoyakbackend/board/controller/BoardController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/BoardController.java
@@ -1,4 +1,27 @@
 package com.example.ahimmoyakbackend.board.controller;
 
+import com.example.ahimmoyakbackend.board.dto.BoardCreateRequestDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateResponseDTO;
+import com.example.ahimmoyakbackend.board.entity.Type;
+import com.example.ahimmoyakbackend.board.service.BoardService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "BoardController")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/v1/board/{boardType}")
+    public ResponseEntity<BoardCreateResponseDTO> createBoard(@RequestBody BoardCreateRequestDTO requestDTO, @PathVariable Type boardType){
+        BoardCreateResponseDTO created = boardService.create(requestDTO,boardType);
+        return ResponseEntity.status(HttpStatus.OK).body(created);
+    }
+
 }
