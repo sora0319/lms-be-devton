7961bc35e2af466fbf00c5bc9e8dbe3760571163
Miru <alfn051@gmail.com>
2024-09-11 17:42:46 +0900

    add : 수료증 뷰 추가

--- src/main/java/com/example/ahimmoyakbackend/global/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ahimmoyakbackend.global.repository;
-
-import com.example.ahimmoyakbackend.auth.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
