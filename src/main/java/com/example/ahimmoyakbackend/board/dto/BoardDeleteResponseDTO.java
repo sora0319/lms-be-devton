c3472bbc9dd8f4a5928975442b10fb32ab80a79e
gomar <ju79318@naver.com>
2024-09-11 16:38:32 +0900

    add : BoardDeleteResponseDTO 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/BoardDeleteResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.ahimmoyakbackend.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDeleteResponseDTO {
+
+    private String msg;
+
+}
