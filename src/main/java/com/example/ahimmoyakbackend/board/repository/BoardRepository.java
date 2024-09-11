a55c89ec898a19522a49d31b8493a69c582983da
gomar <ju79318@naver.com>
2024-09-11 17:17:31 +0900

    feat : findAllByBoardType 추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package com.example.ahimmoyakbackend.board.repository;
 
 import com.example.ahimmoyakbackend.board.entity.Board;
+import com.example.ahimmoyakbackend.board.entity.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    List<Board> findAllByBoardType(Type boardType);
 }
