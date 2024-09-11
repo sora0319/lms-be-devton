d14bf8c785eaa80e6b52a6def3609f18ae787db9
Dowon <tnstjs1919@gmail.com>
2024-09-12 00:49:48 +0900

    mod : AffiliationRepository 에 companyId, affiliationId, departmentId 조회 기능 추가

--- src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Affiliation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
+    List<Affiliation> findByCompanyIdAndAffiliationIdAndDepartmentId(Long companyId, Long affiliationId, Long departmentId);
 }
