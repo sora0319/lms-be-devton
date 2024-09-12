6f6a1c237322d4899d3341834fb377dae33c483f
gomar <ju79318@naver.com>
2024-09-12 15:21:25 +0900

    mod : createDTO 메서드 삭제

--- src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.ahimmoyakbackend.board.repository;
 
 import com.example.ahimmoyakbackend.board.common.BoardType;
 import com.example.ahimmoyakbackend.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,5 @@ import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
-    List<Board> findAllByType(BoardType type);
+    Page<Board> findAllByTypeOrderByCreatedAtDesc(BoardType type,Pageable pageable);
 }
