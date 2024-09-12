42fc73e41f2deae80db654d8e36195b74e25c649
gomar <ju79318@naver.com>
2024-09-12 17:12:28 +0900

    feat : 댓글 생성 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/service/CommentService.java
+++ src/main/java/com/example/ahimmoyakbackend/board/service/CommentService.java
@@ -1,9 +1,30 @@
 package com.example.ahimmoyakbackend.board.service;
 
+import com.example.ahimmoyakbackend.board.dto.CommentCreateRequestDto;
+import com.example.ahimmoyakbackend.board.dto.CommentCreateResponseDto;
+import com.example.ahimmoyakbackend.board.entity.Board;
+import com.example.ahimmoyakbackend.board.entity.Comment;
+import com.example.ahimmoyakbackend.board.repository.BoardRepository;
+import com.example.ahimmoyakbackend.board.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+
+    private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
+
+    public CommentCreateResponseDto create(CommentCreateRequestDto requestDto, Long boardId) {
+        Board board = boardRepository.findById(boardId).orElseThrow(()->new IllegalArgumentException("없는 게시물 입니다."));
+        Comment comment = Comment.builder()
+                .content(requestDto.getContent())
+                .board(board)
+                .build();
+        commentRepository.save(comment);
+        return CommentCreateResponseDto.builder().msg("댓글 작성 성공").build();
+
+    }
+
 }
