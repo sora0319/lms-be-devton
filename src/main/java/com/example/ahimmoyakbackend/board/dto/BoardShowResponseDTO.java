fff703eda67ccffc15e5d8fa4be8e62cabbabdfa
gomar <ju79318@naver.com>
2024-09-12 16:45:06 +0900

    add : BoardShowResponseDTO 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/BoardShowResponseDTO.java
@@ -0,0 +1,34 @@
+package com.example.ahimmoyakbackend.board.dto;
+
+import com.example.ahimmoyakbackend.auth.entity.User;
+import com.example.ahimmoyakbackend.board.common.BoardType;
+import com.example.ahimmoyakbackend.board.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardShowResponseDTO {
+
+//    private String username;
+
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    private BoardType type;
+
+    private LocalDateTime createAt;
+
+    private List<Comment> comments;
+
+}
