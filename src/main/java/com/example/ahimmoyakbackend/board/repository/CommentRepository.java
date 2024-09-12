7b3d252912bee8d971a3fafb93cc01a3953c9881
gomar <ju79318@naver.com>
2024-09-12 19:02:25 +0900

    feat : findAllByBoardId 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/CommentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.board.repository;
 import com.example.ahimmoyakbackend.board.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByBoardId(Long board_id);
 }
