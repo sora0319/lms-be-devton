eab1f6e4462da49f3a076d3d6541c8a2263cdfc7
Dowon <tnstjs1919@gmail.com>
2024-09-14 00:18:32 +0900

    add : companyController 부서 등록 기능 구현

--- src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
+++ src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
@@ -1,15 +1,13 @@
 package com.example.ahimmoyakbackend.company.controller;
 
-import com.example.ahimmoyakbackend.company.dto.CompanyInquiryUserRequestDto;
-import com.example.ahimmoyakbackend.company.dto.CompanyInquiryUserResponseDto;
+import com.example.ahimmoyakbackend.company.dto.CompanyEnrollDepartmentRequestDto;
+import com.example.ahimmoyakbackend.company.dto.CompanyEnrollDepartmentResponseDto;
 import com.example.ahimmoyakbackend.company.service.CompanyService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "CompanyController")
 @RestController
@@ -19,12 +17,12 @@ public class CompanyController {
 
     private final CompanyService companyService;
 
-    @RequestMapping("/supervisor")
-    public ResponseEntity<List<CompanyInquiryUserResponseDto>> getUserList(@RequestParam("companyId") Long companyId,
-                                                                    @RequestParam("affiliationId") Long affiliationId,
-                                                                    @RequestParam("departmentId") Long departmentId
+    @RequestMapping(value = "/v1/supervisor", method = RequestMethod.POST)
+    public ResponseEntity<CompanyEnrollDepartmentResponseDto> enrollDepartment(@RequestParam("companyId") Long companyId,
+                                                                               @RequestParam("affiliationId") Long affiliationId,
+                                                                               @RequestBody CompanyEnrollDepartmentRequestDto requestDto
     ) {
-        List<CompanyInquiryUserResponseDto> userList = companyService.getUserByCompany(companyId, affiliationId, departmentId);
-        return ResponseEntity.status(HttpStatus.OK).body(userList);
+        CompanyEnrollDepartmentResponseDto enrolled = companyService.enroll(companyId, affiliationId, requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(enrolled);
     }
 }
