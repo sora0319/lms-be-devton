29eb84fc9f159ed5ef35015873180e85b1175c1e
gomar <ju79318@naver.com>
2024-09-13 17:08:17 +0900

    feat : 코스 게시물 작성 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/service/CourseBoardService.java
+++ src/main/java/com/example/ahimmoyakbackend/board/service/CourseBoardService.java
@@ -1,7 +1,34 @@
 package com.example.ahimmoyakbackend.board.service;
 
+import com.example.ahimmoyakbackend.board.common.CourseBoardType;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateRequestDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateResponseDTO;
+import com.example.ahimmoyakbackend.board.entity.CourseBoard;
+import com.example.ahimmoyakbackend.board.repository.CourseBoardRepository;
+import com.example.ahimmoyakbackend.course.entity.Course;
+import com.example.ahimmoyakbackend.course.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CourseBoardService {
+
+    private final CourseBoardRepository courseBoardRepository;
+    private final CourseRepository courseRepository;
+
+
+    public BoardCreateResponseDTO create(BoardCreateRequestDTO requestDTO, Long courseId, CourseBoardType type) {
+
+        Course course = courseRepository.findById(courseId).orElseThrow(()->new IllegalArgumentException("잘못된 코스입니다."));
+
+        CourseBoard board = CourseBoard.builder()
+                .title(requestDTO.getTitle())
+                .content(requestDTO.getContent())
+                .type(type)
+                .course(course)
+                .build();
+        courseBoardRepository.save(board);
+        return BoardCreateResponseDTO.builder().msg("게시물 작성 완료").build();
+    }
 }
