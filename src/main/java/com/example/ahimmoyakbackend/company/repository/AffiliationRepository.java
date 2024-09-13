3b2e24c7d12fc4c08acce1794c9ec915ae61ed25
Dowon <tnstjs1919@gmail.com>
2024-09-14 00:33:29 +0900

    add : 부서 삭제 responseDto 생성

--- src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
-    List<Affiliation> findByCompanyIdAndAffiliationIdAndDepartmentId(Long companyId, Long affiliationId, Long departmentId);
+   // List<Affiliation> findByCompanyIdAndAffiliationIdAndDepartmentId(Long companyId, Long affiliationId, Long departmentId);
+
 }
