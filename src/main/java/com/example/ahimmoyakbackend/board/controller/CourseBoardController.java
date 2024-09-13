bcb6856bfa403a21ad64433c4f4fe2cca616c8ae
gomar <ju79318@naver.com>
2024-09-13 17:08:04 +0900

    feat : 코스 게시물 작성 기능 추가

--- src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
@@ -1,13 +1,27 @@
 package com.example.ahimmoyakbackend.board.controller;
 
+import com.example.ahimmoyakbackend.board.common.CourseBoardType;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateRequestDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardCreateResponseDTO;
+import com.example.ahimmoyakbackend.board.service.CourseBoardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "BoardController")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CourseBoardController {
+
+    private final CourseBoardService courseBoardService;
+
+    @PostMapping("/v1/board/{type}/{courseId}")
+    public ResponseEntity<BoardCreateResponseDTO> createBoard(@RequestBody BoardCreateRequestDTO requestDTO, @PathVariable Long courseId,@PathVariable CourseBoardType type){
+        BoardCreateResponseDTO created = courseBoardService.create(requestDTO,courseId,type);
+        return ResponseEntity.status(HttpStatus.OK).body(created);
+    }
+
 }
