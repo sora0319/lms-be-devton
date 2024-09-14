435e6783dd3799b10c7b8fa65f76e3f8d0658a01
Dowon <tnstjs1919@gmail.com>
2024-09-14 22:57:27 +0900

    feat : CompanyUpdateResponseDto 생성

--- src/main/java/com/example/ahimmoyakbackend/company/dto/CompanyUpdateResponseDto.java
+++ src/main/java/com/example/ahimmoyakbackend/company/dto/CompanyUpdateResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.ahimmoyakbackend.company.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyUpdateResponseDto {
+    private String msg;
 }
