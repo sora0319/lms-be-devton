656be8c7b669e1a6eb28b25311a90d9904222016
gomar <ju79318@naver.com>
2024-09-14 23:51:20 +0900

    mod : 코스별 type 별로 게시글 찾는 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/CourseBoardRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/CourseBoardRepository.java
@@ -1,7 +1,12 @@
 package com.example.ahimmoyakbackend.board.repository;
 
+import com.example.ahimmoyakbackend.board.common.BoardType;
 import com.example.ahimmoyakbackend.board.entity.CourseBoard;
+import com.example.ahimmoyakbackend.course.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseBoardRepository extends JpaRepository<CourseBoard, Long> {
+    Page<CourseBoard> findAllByCourseAndTypeOrderByCreatedAtDesc(Course course, BoardType type, Pageable pageable);
 }
