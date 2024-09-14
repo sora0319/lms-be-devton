fe6aada379f2819c783eeeccace643c0477d8172
gomar <ju79318@naver.com>
2024-09-14 21:42:21 +0900

    mod : tag 이름 CourseBoardController 로 수정

--- src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "BoardController")
+@Tag(name = "CourseBoardController")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
