b0567a779e81105758343110376099a5ca8cd976
gomar <ju79318@naver.com>
2024-09-15 02:00:36 +0900

    add : CourseBoardShowResponseDto 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/CourseBoardShowResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.ahimmoyakbackend.board.dto;
+
+import com.example.ahimmoyakbackend.board.common.BoardType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseBoardShowResponseDto {
+
+    private String courseTitle;
+
+    private String username;
+
+    private String title;
+
+    private String content;
+
+    private BoardType type;
+
+    private LocalDateTime createAt;
+
+    private List<CommentOnBoardResponseDto> comments;
+}
