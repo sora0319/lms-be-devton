b6073e63c56bc421f9aee4c02e0e069cf2487df7
Dowon <tnstjs1919@gmail.com>
2024-09-14 23:05:23 +0900

    mod : CompanyUpdateResponseDto 의 toDto 구현

--- src/main/java/com/example/ahimmoyakbackend/company/dto/CompanyUpdateResponseDto.java
+++ src/main/java/com/example/ahimmoyakbackend/company/dto/CompanyUpdateResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.ahimmoyakbackend.company.dto;
 
+import com.example.ahimmoyakbackend.company.entity.Company;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanyUpdateResponseDto {
-    private String msg;
+    private String name;
+    private String ownerName;
+    private String businessNumber;
+    private String email;
+    private String phone;
+
+    public static CompanyUpdateResponseDto toDto(Company company) {
+        return CompanyUpdateResponseDto.builder()
+                .name(company.getName())
+                .ownerName(company.getOwnerName())
+                .businessNumber(company.getBusinessNumber())
+                .email(company.getEmail())
+                .phone(company.getPhone())
+                .build();
+
+    }
 }
