b64ca9f208eaf436b1e0b01060db69f66eca1099
gomar <ju79318@naver.com>
2024-09-15 02:02:05 +0900

    mod : findAllByCourseBoardId 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/CourseCommentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/CourseCommentRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.board.repository;
 import com.example.ahimmoyakbackend.board.entity.CourseComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseCommentRepository extends JpaRepository<CourseComment, Long> {
+    List<CourseComment> findAllByCourseBoardId(Long courseBoardId);
 }
