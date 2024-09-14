6cc8ff130685b431f0788c106c74c4564ee08129
gomar <ju79318@naver.com>
2024-09-14 21:30:11 +0900

    mod : CourseBoardType 를 BoardType 로 변경

--- src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/CourseBoardController.java
@@ -1,13 +1,17 @@
 package com.example.ahimmoyakbackend.board.controller;
 
+import com.example.ahimmoyakbackend.board.common.BoardType;
 import com.example.ahimmoyakbackend.board.common.CourseBoardType;
 import com.example.ahimmoyakbackend.board.dto.BoardCreateRequestDTO;
 import com.example.ahimmoyakbackend.board.dto.BoardCreateResponseDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardUpdateRequestDTO;
+import com.example.ahimmoyakbackend.board.dto.BoardUpdateResponseDTO;
 import com.example.ahimmoyakbackend.board.service.CourseBoardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "BoardController")
@@ -19,7 +23,7 @@ public class CourseBoardController {
     private final CourseBoardService courseBoardService;
 
     @PostMapping("/v1/board/{type}/{courseId}")
-    public ResponseEntity<BoardCreateResponseDTO> createBoard(@RequestBody BoardCreateRequestDTO requestDTO, @PathVariable Long courseId,@PathVariable CourseBoardType type){
+    public ResponseEntity<BoardCreateResponseDTO> createBoard(@RequestBody BoardCreateRequestDTO requestDTO, @PathVariable Long courseId,@PathVariable BoardType type){
         BoardCreateResponseDTO created = courseBoardService.create(requestDTO,courseId,type);
         return ResponseEntity.status(HttpStatus.OK).body(created);
     }
