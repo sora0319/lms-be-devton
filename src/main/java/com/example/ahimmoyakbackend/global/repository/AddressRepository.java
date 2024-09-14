ac5997306187e20a60601ec3cf26c9cfe04f3664
Dowon <tnstjs1919@gmail.com>
2024-09-15 00:16:48 +0900

    feat : AddressRepository 에 findByUserId 생성

--- src/main/java/com/example/ahimmoyakbackend/global/repository/AddressRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/global/repository/AddressRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.global.repository;
 import com.example.ahimmoyakbackend.global.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findByUserId(Long userId);
 }
