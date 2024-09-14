83f27e21805ca21b05507dc66ff5348df8a11426
Dowon <tnstjs1919@gmail.com>
2024-09-14 23:46:21 +0900

    mod : AffiliationRepository 의 findAllByCompanyIdAndDepartmentId 메서드 변경

--- src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
-   // List<Affiliation> findByCompanyIdAndAffiliationIdAndDepartmentId(Long companyId, Long affiliationId, Long departmentId);
-
+    List<Affiliation> findAllByCompanyIdAndDepartmentId(Long companyId, Long departmentId);
 }
