a6968bc11dcb7fde502c0973905f5fae8430855b
Dowon <tnstjs1919@gmail.com>
2024-09-14 21:31:43 +0900

    mod : DepartmentRepository 에 findByCompanyId 추가

--- src/main/java/com/example/ahimmoyakbackend/company/repository/DepartmentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/DepartmentRepository.java
@@ -2,6 +2,8 @@ package com.example.ahimmoyakbackend.company.repository;
 
 import com.example.ahimmoyakbackend.company.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findByCompanyId(Long companyId);
 }
