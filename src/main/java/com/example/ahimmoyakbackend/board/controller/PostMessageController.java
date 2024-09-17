b6ac2bd020bb2b5c72420302cd03ccc67283082e
gomar <ju79318@naver.com>
2024-09-17 11:41:43 +0900

    add : PostMessageController 파일 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/PostMessageController.java
@@ -0,0 +1,14 @@
+package com.example.ahimmoyakbackend.board.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "PostMessageController")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PostMessageController {
+
+
+}
