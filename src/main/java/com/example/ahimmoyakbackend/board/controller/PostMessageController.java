7654e0ac25cf4b27324bbd2efb7d16b1de6453b8
gomar <ju79318@naver.com>
2024-09-17 20:28:04 +0900

    feat : 보낸 쪽지 조회 기능 추가

--- src/main/java/com/example/ahimmoyakbackend/board/controller/PostMessageController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/PostMessageController.java
@@ -1,12 +1,15 @@
 package com.example.ahimmoyakbackend.board.controller;
 
-import com.example.ahimmoyakbackend.board.dto.PostMessageRequestDto;
-import com.example.ahimmoyakbackend.board.dto.PostMessageResponseDto;
+import com.example.ahimmoyakbackend.auth.entity.User;
+import com.example.ahimmoyakbackend.board.dto.SendPostMessageRequestDto;
+import com.example.ahimmoyakbackend.board.dto.SendPostMessageResponseDto;
+import com.example.ahimmoyakbackend.board.dto.ReceivePostMessageResponseDto;
 import com.example.ahimmoyakbackend.board.service.PostMessageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "PostMessageController")
@@ -18,8 +21,15 @@ public class PostMessageController {
     private final PostMessageService postMessageService;
 
     @PostMapping("/v1/message")
-    public ResponseEntity<PostMessageResponseDto> sendMessage(@RequestBody PostMessageRequestDto messageRequestDto) {
-        PostMessageResponseDto sendMessage = postMessageService.write(messageRequestDto);
+    public ResponseEntity<SendPostMessageResponseDto> sendMessage(@RequestBody SendPostMessageRequestDto messageRequestDto) {
+        SendPostMessageResponseDto sendMessage = postMessageService.write(messageRequestDto);
       return ResponseEntity.status(HttpStatus.OK).body(sendMessage);
+    }
+
+    @GetMapping("/v1/message/send")
+    public ResponseEntity<ReceivePostMessageResponseDto> receiveInquiry(@AuthenticationPrincipal User user, @RequestParam(defaultValue = "1") int page,
+                                                                        @RequestParam(defaultValue = "10") int size){
+        ReceivePostMessageResponseDto receiveMessage = postMessageService.receiveInquriy(user,page,size);
+        return ResponseEntity.status(HttpStatus.OK).body(receiveMessage);
     }
 }
