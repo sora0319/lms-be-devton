d873bed8fcb327e19c71bf2cc332a3af3f1b7edd
gomar <ju79318@naver.com>
2024-09-17 11:43:50 +0900

    feat : 메세지 보내기 기능 생성

--- src/main/java/com/example/ahimmoyakbackend/board/controller/PostMessageController.java
+++ src/main/java/com/example/ahimmoyakbackend/board/controller/PostMessageController.java
@@ -1,7 +1,12 @@
 package com.example.ahimmoyakbackend.board.controller;
 
+import com.example.ahimmoyakbackend.board.dto.PostMessageRequestDto;
+import com.example.ahimmoyakbackend.board.dto.PostMessageResponseDto;
+import com.example.ahimmoyakbackend.board.service.PostMessageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "PostMessageController")
@@ -10,5 +15,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PostMessageController {
 
+    private final PostMessageService postMessageService;
 
+    @PostMapping("/v1/message")
+    public ResponseEntity<PostMessageResponseDto> sendMessage(@RequestBody PostMessageRequestDto messageRequestDto) {
+        PostMessageResponseDto sendMessage = postMessageService.write(messageRequestDto);
+      return ResponseEntity.status(HttpStatus.OK).body(sendMessage);
+    }
 }
