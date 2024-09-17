c8c5c7d62a92533410358d2f79e260b7bd8005b4
gomar <ju79318@naver.com>
2024-09-17 20:28:32 +0900

    mod : findPostMessagesBySenderOrderByCreatedAtDesc 추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/PostMessageRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/PostMessageRepository.java
@@ -1,7 +1,11 @@
 package com.example.ahimmoyakbackend.board.repository;
 
+import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.board.entity.PostMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostMessageRepository extends JpaRepository<PostMessage, Long> {
+    Page<PostMessage> findPostMessagesBySenderOrderByCreatedAtDesc(User user, Pageable pageable);
 }
