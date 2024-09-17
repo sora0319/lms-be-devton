cb92ecdc05ef2c3707d55e8dcc1559bad020b463
gomar <ju79318@naver.com>
2024-09-17 20:22:35 +0900

    add : ReceivePostMessageResponseDto 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/ReceivePostMessageResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.ahimmoyakbackend.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReceivePostMessageResponseDto {
+
+    private List<PostMessageResponseDto> messages;
+
+    private Pagination pages;
+
+}
