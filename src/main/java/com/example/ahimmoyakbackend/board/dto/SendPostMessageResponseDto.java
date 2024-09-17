f7bb27c5c80cc4a0180d63f857ac9041df9608a9
gomar <ju79318@naver.com>
2024-09-17 20:22:29 +0900

    add : SendPostMessageResponseDto 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/SendPostMessageResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.ahimmoyakbackend.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SendPostMessageResponseDto {
+
+    private String msg;
+
+}
