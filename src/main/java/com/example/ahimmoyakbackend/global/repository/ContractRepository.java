1e8097b67f6e95b30b77aeeb04e7f48a3fcf7a40
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-18 16:38:27 +0900

    mod : ContractRepository 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/global/repository/ContractRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/global/repository/ContractRepository.java
@@ -1,7 +1,13 @@
 package com.example.ahimmoyakbackend.global.repository;
 
+import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.company.entity.Contract;
+import com.example.ahimmoyakbackend.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContractRepository extends JpaRepository<Contract, Long> {
+
+    Optional<Contract> findByCourseAndEnrollments_User(Course course, User user);
 }
