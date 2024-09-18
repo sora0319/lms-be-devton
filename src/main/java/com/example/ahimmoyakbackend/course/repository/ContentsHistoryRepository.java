e0995e8b76ee437a945ab141c28ff8198b35f903
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-18 16:35:01 +0900

    mod : ContentsHistoryRepository 메소드 추가

--- src/main/java/com/example/ahimmoyakbackend/course/repository/ContentsHistoryRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/course/repository/ContentsHistoryRepository.java
@@ -1,10 +1,16 @@
 package com.example.ahimmoyakbackend.course.repository;
 
+import com.example.ahimmoyakbackend.auth.entity.User;
+import com.example.ahimmoyakbackend.course.entity.Contents;
 import com.example.ahimmoyakbackend.course.entity.ContentsHistory;
+import com.example.ahimmoyakbackend.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ContentsHistoryRepository extends JpaRepository<ContentsHistory, Long> {
-    ContentsHistory findByContents_IdAndEnrollment_User_Id(Long contentsId, Long userId);
+    List<ContentsHistory> findByEnrollment_UserAndContents_Curriculum_Course(User user, Course course);
+
+    Optional<ContentsHistory> findByContentsAndEnrollment_User(Contents contents, User user);
 }
