2e9e8db6b0df8866984e5b7616839b04ea557d00
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-18 16:42:39 +0900

    mod : CourseService 에 Inquiry 메소드 수정

--- src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
+++ src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.example.ahimmoyakbackend.course.service;
 
+import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.company.entity.Contract;
 import com.example.ahimmoyakbackend.contents.dto.ContentsInquiryResponseDTO;
 import com.example.ahimmoyakbackend.course.common.ContentsHistoryState;
@@ -7,87 +8,64 @@ import com.example.ahimmoyakbackend.course.dto.CourseDetailsInquiryResponseDTO;
 import com.example.ahimmoyakbackend.course.entity.Contents;
 import com.example.ahimmoyakbackend.course.entity.ContentsHistory;
 import com.example.ahimmoyakbackend.course.entity.Course;
+import com.example.ahimmoyakbackend.course.entity.Curriculum;
 import com.example.ahimmoyakbackend.course.repository.ContentsHistoryRepository;
-import com.example.ahimmoyakbackend.course.repository.ContentsRepository;
 import com.example.ahimmoyakbackend.course.repository.CourseRepository;
 import com.example.ahimmoyakbackend.curriculum.dto.CurriculumInquiryResponseDTO;
-import com.example.ahimmoyakbackend.global.entity.Timestamped;
 import com.example.ahimmoyakbackend.global.repository.ContractRepository;
-import com.example.ahimmoyakbackend.institution.entity.Tutor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
     private final ContentsHistoryRepository contentsHistoryRepository;
-    private final ContentsRepository contentsRepository;
     private final ContractRepository contractRepository;
 
 
     @Transactional
-    public CourseDetailsInquiryResponseDTO Inquiry(Long userId, Long courseId, ContentsHistoryState state) {
+    public CourseDetailsInquiryResponseDTO Inquiry(User user, Long courseId) {
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("코스가 없습니다."));
+        Contract contract = contractRepository.findByCourseAndEnrollments_User(course, user).orElseThrow(() -> new IllegalArgumentException("컨텐츠가 없습니다."));
+        CourseDetailsInquiryResponseDTO courseDto = CourseDetailsInquiryResponseDTO.from(course, contract);
 
-        Course findCourseId = courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("해당 코스를 찾을 수 없습니다."));
+        List<CurriculumInquiryResponseDTO> curriculumDTOList = new ArrayList<>();
+        int courseTotalContents = 0;
+        int courseCompletedContents = 0;
 
-        Tutor tutor = findCourseId.getTutor();
+        for (Curriculum curriculum : course.getCurriculumList()) {
 
-        List<CurriculumInquiryResponseDTO> curriculumResponseDTOList = findCourseId.getCurriculumList().stream()
-            .map(curriculum -> {
-                double progress = calculateProgress(curriculum.getContentsList(), userId);
+            CurriculumInquiryResponseDTO curriculumDto = CurriculumInquiryResponseDTO.from(curriculum);
+            List<ContentsInquiryResponseDTO> contentsDTOList = new ArrayList<>();
 
-                return CurriculumInquiryResponseDTO.builder()
-                        .curriculumTitle(curriculum.getTitle())
-                        .curriculumId(curriculum.getId())
-                        .curriculumIdx(curriculum.getIdx())
-                        .contentsProgress(progress)
-                        .contentList(curriculum.getContentsList().stream()
-                                .map(contents -> ContentsInquiryResponseDTO.builder()
-                                        .contentsId(contents.getId())
-                                        .title(contents.getTitle())
-                                        .contentsIdx(contents.getIdx())
-                                        .build())
-                                .collect(Collectors.toList()))
-                        .build();
-            }).collect(Collectors.toList());
-        return CourseDetailsInquiryResponseDTO.builder()
-                .courseTitle(findCourseId.getTitle())
-                .tutorName(tutor.getUser().getName())
-                .courseProgress(state)
-                .startDate(new Timestamped())
-                .endDate(new Timestamped())
-                .courseImage("")
-                .introduction(findCourseId.getIntroduction())
-                .curriculumInquiryResponseDTO(curriculumResponseDTOList)
-                .build();
+            int completedContents = 0;
 
-    }
+            for (Contents contents : curriculum.getContentsList()) {
 
-    // 유저의 콘텐츠 상태
-    public ContentsHistoryState getStateByUserAndCourse(Long userId, Long contentId) {
-        ContentsHistory contentsHistory = contentsHistoryRepository.findByContents_IdAndEnrollment_User_Id(userId, contentId);
+                ContentsHistory history = contentsHistoryRepository.findByContentsAndEnrollment_User(contents, user).orElseThrow(()-> new IllegalArgumentException("컨텐츠의 히스토리가 없습니다."));
 
-        return contentsHistory.getState();
-    }
+                ContentsInquiryResponseDTO contentsDTO = ContentsInquiryResponseDTO.from(contents, history.getState());
 
-    // 진행률 계산
-    public double calculateProgress(List<Contents> contentsList, Long userId) {
-        int totalContents = contentsList.size();
-        long count = contentsList.stream()
-                .map(contents -> contentsHistoryRepository.findByContents_IdAndEnrollment_User_Id(userId, contents.getId()))
-                .filter(contentsHistory -> contentsHistory.getState() == ContentsHistoryState.COMPLETED)
-                .count();
+                courseTotalContents++;
 
-        if (totalContents == 0) {
-            return 0;
-        } else {
-
-            return (double) count / totalContents * 100;  //  총 contentList 사이즈에 Completed 인 것을 나눠서 백분율
+                if (history.getState().equals(ContentsHistoryState.COMPLETED)) {
+                    courseCompletedContents++;
+                    completedContents++;
+                }
+                contentsDTOList.add(contentsDTO);
+            }
+            curriculumDto.setCompletedContents(completedContents)
+                    .setContents(contentsDTOList);
+            curriculumDTOList.add(curriculumDto);
         }
+        courseDto.setCompletedContents(courseCompletedContents)
+                .setTotalContents(courseTotalContents)
+                .setCurriculums(curriculumDTOList);
+        return courseDto;
+
     }
 }
