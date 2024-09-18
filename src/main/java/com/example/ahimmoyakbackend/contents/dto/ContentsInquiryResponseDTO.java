b94137cf7f56871f1a51caa4fce496768eb429fc
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-18 16:35:20 +0900

    mod : from 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/contents/dto/ContentsInquiryResponseDTO.java
+++ src/main/java/com/example/ahimmoyakbackend/contents/dto/ContentsInquiryResponseDTO.java
@@ -1,6 +1,8 @@
 package com.example.ahimmoyakbackend.contents.dto;
 
 import com.example.ahimmoyakbackend.course.common.ContentType;
+import com.example.ahimmoyakbackend.course.common.ContentsHistoryState;
+import com.example.ahimmoyakbackend.course.entity.Contents;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +17,15 @@ public class ContentsInquiryResponseDTO {
     private String title;
     private Integer contentsIdx;
     private ContentType type;
+    private ContentsHistoryState contentsState;
 
+    public static ContentsInquiryResponseDTO from(Contents contents, ContentsHistoryState state) {
+        return ContentsInquiryResponseDTO.builder()
+                .contentsId(contents.getId())
+                .title(contents.getTitle())
+                .contentsIdx(contents.getIdx())
+                .type(contents.getType())
+                .contentsState(state)
+                .build();
+    }
 }
