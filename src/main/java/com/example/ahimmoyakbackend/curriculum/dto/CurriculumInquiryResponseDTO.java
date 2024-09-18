d38211c50dc766b5cb94c046a65d6170a35d4290
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-18 16:43:00 +0900

    mod : CurriculumInquiryResponseDTO 객체와 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/curriculum/dto/CurriculumInquiryResponseDTO.java
+++ src/main/java/com/example/ahimmoyakbackend/curriculum/dto/CurriculumInquiryResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.ahimmoyakbackend.curriculum.dto;
 
 import com.example.ahimmoyakbackend.contents.dto.ContentsInquiryResponseDTO;
+import com.example.ahimmoyakbackend.course.entity.Curriculum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,11 +14,27 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CurriculumInquiryResponseDTO {
-
     private Long curriculumId;
-    private Integer curriculumIdx;
-    private String curriculumTitle;
-    private Double contentsProgress;
-    private List<ContentsInquiryResponseDTO> contentList;
+    private Integer totalContents;
+    private Integer completedContents;  // 진행률 계산을 위한 총 콘텐츠 수
+    private Integer curriculumIdx;  // 진행률 계산을 위한 완료된 콘텐츠 수
+    private List<ContentsInquiryResponseDTO> contents;
 
+    public static CurriculumInquiryResponseDTO from(Curriculum curriculum) {
+        return CurriculumInquiryResponseDTO.builder()
+                .curriculumId(curriculum.getId())
+                .totalContents(curriculum.getContentsList().size())
+                .curriculumIdx(curriculum.getIdx())
+                .build();
+    }
+
+    public CurriculumInquiryResponseDTO setCompletedContents(int completedContents) {
+        this.completedContents = completedContents;
+        return this;
+    }
+
+    public CurriculumInquiryResponseDTO setContents(List<ContentsInquiryResponseDTO> contentsInquiryResponseDTOList) {
+        this.contents = contentsInquiryResponseDTOList;
+        return this;
+    }
 }
