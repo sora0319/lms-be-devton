e1aefd6d933c99594d68c5c1d6bfd862a1ecbcec
sora0319 <sojeoung536985@gmail.com>
2024-09-20 05:37:37 +0900

    mod : UserDetailsServiceImpl에 역할이 생김에 따라 생성자 변수 수정

--- src/main/java/com/example/ahimmoyakbackend/auth/config/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/ahimmoyakbackend/auth/config/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.ahimmoyakbackend.auth.config.security;
 
+import com.example.ahimmoyakbackend.auth.common.UserRole;
 import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findUserByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-        return new UserDetailsImpl(user, user.getUsername());
+        return new UserDetailsImpl(user, user.getUsername(), List.of(user.getRole()));
     }
 }
