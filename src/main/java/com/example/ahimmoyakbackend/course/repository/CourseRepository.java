d5707ce4f7e4d2c2a86fcced349b08ecd4b66981
nowgnodeel <nowgnodeel123@gmail.com>
2024-09-19 19:36:24 +0900

    add : 코스 리스트 카테고리별로 페이징 처리해서 조회하기

--- src/main/java/com/example/ahimmoyakbackend/course/repository/CourseRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/course/repository/CourseRepository.java
@@ -1,7 +1,13 @@
 package com.example.ahimmoyakbackend.course.repository;
 
+import com.example.ahimmoyakbackend.course.common.CourseCategory;
 import com.example.ahimmoyakbackend.course.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    Page<Course> findAllByCategory(CourseCategory category, Pageable pageable);
 }
