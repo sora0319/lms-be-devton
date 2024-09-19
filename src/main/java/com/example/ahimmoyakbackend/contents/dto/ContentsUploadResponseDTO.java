d6449d8adafe9330053d807344f3a649f9d0f48f
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-19 15:48:08 +0900

    add : ContentsUploadResponseDTO 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/contents/dto/ContentsUploadResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.ahimmoyakbackend.contents.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContentsUploadResponseDTO {
+
+    private String msg;
+
+}
