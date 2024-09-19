babc0b9f7264b15545fd377edd733457633380bf
nowgnodeel <nowgnodeel123@gmail.com>
2024-09-19 20:58:03 +0900

    add : 강사 대시보드에서 사용할 코스리스트의 DTO 추가

---
+++ src/main/java/com/example/ahimmoyakbackend/course/dto/TutorGetCourseListResponseDTO.java
@@ -0,0 +1,19 @@
+package com.example.ahimmoyakbackend.course.dto;
+
+import com.example.ahimmoyakbackend.course.common.CourseCategory;
+import com.example.ahimmoyakbackend.global.entity.Image;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TutorGetCourseListResponseDTO {
+    private Long id;
+    private String title;
+    private Image image;
+    private CourseCategory category;
+}
