34af2c8dad27823cadd297e9f59c701ad60e658d
nowgnodeel <nowgnodeel123@gmail.com>
2024-09-19 20:58:55 +0900

    add : username 으로 Tutor 조회

--- src/main/java/com/example/ahimmoyakbackend/institution/repository/TutorRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/institution/repository/TutorRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.institution.repository;
 import com.example.ahimmoyakbackend.institution.entity.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
+    Optional<Tutor> findByUserName(String username);
 }
