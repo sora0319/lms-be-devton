9dc22c5d02b445025c99b8eeaee554a2db789b43
gomar <ju79318@naver.com>
2024-09-19 15:47:47 +0900

    rename : ReceivePostMessageResponseDto 에서 PostMessageInquiryResponseDto 로 이름 변경

--- src/main/java/com/example/ahimmoyakbackend/board/dto/PostMessageInquiryResponseDto.java
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/PostMessageInquiryResponseDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ReceivePostMessageResponseDto {
+public class PostMessageInquiryResponseDto {
 
     private List<PostMessageResponseDto> messages;
 
