87c72d54b9fc609bb45c35909c93099f383fa568
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-21 22:14:45 +0900

    add : CurriculumChangeOrderResponseDTO 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/curriculum/dto/CurriculumChangeOrderResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.ahimmoyakbackend.curriculum.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurriculumChangeOrderResponseDTO {
+
+    private String msg;
+}
