927f6666a47128bdd4884cc856d706264425433d
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-21 22:14:28 +0900

    mod : CourseService CRUD 구현

--- src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
+++ src/main/java/com/example/ahimmoyakbackend/course/service/CourseService.java
@@ -1,10 +1,12 @@
 package com.example.ahimmoyakbackend.course.service;
 
+import ch.qos.logback.core.rolling.DefaultTimeBasedFileNamingAndTriggeringPolicy;
+import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
 import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.company.entity.Contract;
 import com.example.ahimmoyakbackend.contents.dto.ContentsInquiryResponseDTO;
 import com.example.ahimmoyakbackend.course.common.ContentsHistoryState;
-import com.example.ahimmoyakbackend.course.dto.CourseDetailsInquiryResponseDTO;
+import com.example.ahimmoyakbackend.course.dto.*;
 import com.example.ahimmoyakbackend.course.entity.Contents;
 import com.example.ahimmoyakbackend.course.entity.ContentsHistory;
 import com.example.ahimmoyakbackend.course.entity.Course;
@@ -12,10 +14,15 @@ import com.example.ahimmoyakbackend.course.entity.Curriculum;
 import com.example.ahimmoyakbackend.course.repository.ContentsHistoryRepository;
 import com.example.ahimmoyakbackend.course.repository.CourseRepository;
 import com.example.ahimmoyakbackend.curriculum.dto.CurriculumInquiryResponseDTO;
+import com.example.ahimmoyakbackend.global.entity.Image;
 import com.example.ahimmoyakbackend.global.repository.ContractRepository;
+import com.example.ahimmoyakbackend.global.repository.ImageRepository;
+import com.example.ahimmoyakbackend.institution.entity.Tutor;
+import com.example.ahimmoyakbackend.institution.repository.TutorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,10 +32,11 @@ public class CourseService {
     private final CourseRepository courseRepository;
     private final ContentsHistoryRepository contentsHistoryRepository;
     private final ContractRepository contractRepository;
+    private final TutorRepository tutorRepository;
 
 
     @Transactional
-    public CourseDetailsInquiryResponseDTO Inquiry(User user, Long courseId) {
+    public CourseDetailsInquiryResponseDTO inquiry(User user, Long courseId) {
         Course course = courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("코스가 없습니다."));
         Contract contract = contractRepository.findByCourseAndEnrollments_User(course, user).orElseThrow(() -> new IllegalArgumentException("컨텐츠가 없습니다."));
         CourseDetailsInquiryResponseDTO courseDto = CourseDetailsInquiryResponseDTO.from(course, contract);
@@ -46,7 +54,7 @@ public class CourseService {
 
             for (Contents contents : curriculum.getContentsList()) {
 
-                ContentsHistory history = contentsHistoryRepository.findByContentsAndEnrollment_User(contents, user).orElseThrow(()-> new IllegalArgumentException("컨텐츠의 히스토리가 없습니다."));
+                ContentsHistory history = contentsHistoryRepository.findByContentsAndEnrollment_User(contents, user).orElseThrow(() -> new IllegalArgumentException("컨텐츠의 히스토리가 없습니다."));
 
                 ContentsInquiryResponseDTO contentsDTO = ContentsInquiryResponseDTO.from(contents, history.getState());
 
@@ -67,5 +75,51 @@ public class CourseService {
                 .setCurriculums(curriculumDTOList);
         return courseDto;
 
+    }
+
+    public CourseCreateResponseDTO create(CourseCreateRequestDTO dto) {
+        Tutor tutor = tutorRepository.findByUser_Name(dto.getTutorName());
+        Course course = courseRepository.findById(dto.getId()).orElseThrow(()-> new IllegalArgumentException("코스가 없습니다."));
+
+
+        Course newCourse = Course.builder()
+                .title(dto.getTitle())
+                .introduction(dto.getIntroduction())
+                .image(course.getImage())
+                .category(dto.getCategory())
+                .type(dto.getType())
+                .tutor(tutor)
+                .build();
+
+        courseRepository.save(newCourse);
+
+        return CourseCreateResponseDTO.builder()
+                .msg("코스 생성 성공")
+                .build();
+
+    }
+
+    public CourseModifyResponseDTO modify(Long courseId, CourseModifyRequestDTO dto) {
+
+        Course course = courseRepository.findById(courseId).orElseThrow(()-> new IllegalArgumentException("코스가 존재 하지 않습니다."));
+
+        course.patch(dto, courseId);
+
+        return CourseModifyResponseDTO.builder()
+                .msg("수정 되었습니다.")
+                .build();
+
+    }
+
+
+    public CourseDeleteResponseDTO delete(Long courseId) {
+
+        Course course = courseRepository.findById(courseId).orElseThrow(()-> new IllegalArgumentException("코스가 존재 하지 않습니다."));
+
+        courseRepository.delete(course);
+
+        return CourseDeleteResponseDTO.builder()
+                .msg("삭제 되었습니다.")
+                .build();
     }
 }
