9ce74221761fb9a497cb8c3e7f9d77005f650a85
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-21 22:15:17 +0900

    mod : curriculumChangeOrder 메소드 구현

---
+++ src/main/java/com/example/ahimmoyakbackend/curriculum/controller/CurriculumController.java
@@ -0,0 +1,35 @@
+package com.example.ahimmoyakbackend.curriculum.controller;
+
+import com.example.ahimmoyakbackend.curriculum.dto.CurriculumChangeOrderResponseDTO;
+import com.example.ahimmoyakbackend.curriculum.service.CurriculumService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1")
+public class CurriculumController {
+
+    private final CurriculumService curriculumService;
+
+    @PatchMapping("/changeOrder")
+    public ResponseEntity<CurriculumChangeOrderResponseDTO> curriculumChangeOrder(
+            @RequestParam Long courseId,
+            @RequestParam Long target1,
+            @RequestParam Long target2
+    ) {
+        CurriculumChangeOrderResponseDTO responseDTO = curriculumService.changeOrder(courseId, target1, target2);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+
+
+    }
+
+
+}
