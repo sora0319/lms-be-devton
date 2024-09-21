fd5b9d8b8564253debc40f1f1099fd24697de6ad
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-21 22:16:23 +0900

    mod : findByUser_Name 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/institution/repository/TutorRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/institution/repository/TutorRepository.java
@@ -4,4 +4,5 @@ import com.example.ahimmoyakbackend.institution.entity.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
+    Tutor findByUser_Name(String tutorName);
 }
