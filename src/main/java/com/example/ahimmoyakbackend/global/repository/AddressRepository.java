cf425addfe1903acdabbfe1d657c9367ff6deb5f
Dowon <tnstjs1919@gmail.com>
2024-09-22 02:24:31 +0900

    mod : findByUserId 를 List 로 변경

--- src/main/java/com/example/ahimmoyakbackend/global/repository/AddressRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/global/repository/AddressRepository.java
@@ -3,8 +3,8 @@ package com.example.ahimmoyakbackend.global.repository;
 import com.example.ahimmoyakbackend.global.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    Optional<Address> findByUserId(Long userId);
+    List<Address> findByUser_Id(Long id);
 }
