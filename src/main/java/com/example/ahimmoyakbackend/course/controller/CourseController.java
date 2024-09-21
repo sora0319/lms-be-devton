50630b1806da1732b191bb5292adb2ef664139c6
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-21 22:09:26 +0900

    feat : Course CRUD메소드 구현

--- src/main/java/com/example/ahimmoyakbackend/course/controller/CourseController.java
+++ src/main/java/com/example/ahimmoyakbackend/course/controller/CourseController.java
@@ -1,16 +1,13 @@
 package com.example.ahimmoyakbackend.course.controller;
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
-import com.example.ahimmoyakbackend.course.common.ContentsHistoryState;
-import com.example.ahimmoyakbackend.course.dto.CourseDetailsInquiryResponseDTO;
+import com.example.ahimmoyakbackend.course.dto.*;
 import com.example.ahimmoyakbackend.course.service.CourseService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -20,10 +17,37 @@ public class CourseController {
     private final CourseService courseService;
 
     @GetMapping("/course/{courseId}")
-    public ResponseEntity<CourseDetailsInquiryResponseDTO> InquiryCourse(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long courseId) {
+    public ResponseEntity<CourseDetailsInquiryResponseDTO> inquiryCourse(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long courseId) {
 
+        CourseDetailsInquiryResponseDTO responseDTO = courseService.inquiry(userDetails.getUser(), courseId);
 
-        CourseDetailsInquiryResponseDTO responseDTO = courseService.Inquiry(userDetails.getUser(), courseId);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+
+    }
+
+    @PostMapping("/course")
+    public ResponseEntity<CourseCreateResponseDTO> createCourse(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CourseCreateRequestDTO dto) {
+
+        CourseCreateResponseDTO responseDTO = courseService.create(dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    ;
+
+    @PatchMapping("/update")
+    public ResponseEntity<CourseModifyResponseDTO> modifyCourse(@RequestParam Long courseId, CourseModifyRequestDTO dto) {
+
+        CourseModifyResponseDTO responseDTO = courseService.modify(courseId, dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<CourseDeleteResponseDTO> deleteCourse(@RequestParam Long courseId) {
+
+        CourseDeleteResponseDTO responseDTO = courseService.delete(courseId);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
 
