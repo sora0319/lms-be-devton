625d1e175da892ee3451ad7de75c85a50a9bdd8b
nowgnodeel123 <nowgnodeel123@gmail.com>
2024-09-23 20:18:50 +0900

    mod : Contract -> CourseProvide 이름 변경

--- src/main/java/com/example/ahimmoyakbackend/company/repository/ContractRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ahimmoyakbackend.company.repository;
-
-import com.example.ahimmoyakbackend.company.entity.Contract;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractRepository extends JpaRepository<Contract, Long> {
-}
---
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/CourseProvideRepository.java
@@ -0,0 +1,7 @@
+package com.example.ahimmoyakbackend.company.repository;
+
+import com.example.ahimmoyakbackend.company.entity.CourseProvide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseProvideRepository extends JpaRepository<CourseProvide, Long> {
+}
