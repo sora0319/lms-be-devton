1cc95f2c03124c4a3d40c82a6c0eba4a9ac8f758
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-23 19:10:48 +0900

    mod : findAllByCompanyAndCourse_Institution 생성

--- src/main/java/com/example/ahimmoyakbackend/company/repository/ContractRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/ContractRepository.java
@@ -1,13 +1,19 @@
 package com.example.ahimmoyakbackend.company.repository;
 
 import com.example.ahimmoyakbackend.auth.entity.User;
+import com.example.ahimmoyakbackend.company.entity.Company;
 import com.example.ahimmoyakbackend.company.entity.Contract;
 import com.example.ahimmoyakbackend.course.entity.Course;
+import com.example.ahimmoyakbackend.institution.entity.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     Optional<Contract> findByCourseAndEnrollments_User(Course course, User user);
+
+    List<Contract> findAllByCompanyAndCourse_Institution(Company company, Institution institution);
 }
