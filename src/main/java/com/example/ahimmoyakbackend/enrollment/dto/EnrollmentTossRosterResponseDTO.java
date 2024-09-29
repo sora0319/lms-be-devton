dffc438de29d98689ecc2e1c2c9ffb8fca2ebc3c
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-29 14:35:13 +0900

    add : EnrollmentTossRosterResponseDTO 생성

---
+++ src/main/java/com/example/ahimmoyakbackend/enrollment/dto/EnrollmentTossRosterResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.ahimmoyakbackend.enrollment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnrollmentTossRosterResponseDTO {
+
+    private String msg;
+
+}
