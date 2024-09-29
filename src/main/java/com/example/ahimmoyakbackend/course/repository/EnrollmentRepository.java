723c014356519a3df2b8c545dae278eb7053e7cd
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-09-29 14:34:32 +0900

    mod : findByContract_Company_IdAndContract_Course_Id 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
@@ -1,7 +1,13 @@
 package com.example.ahimmoyakbackend.course.repository;
 
+import com.example.ahimmoyakbackend.company.entity.Company;
+import com.example.ahimmoyakbackend.course.entity.Course;
 import com.example.ahimmoyakbackend.course.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+    Enrollment findByContract_Company_IdAndContract_Course_Id(Long company_id, Long course_id);
 }
