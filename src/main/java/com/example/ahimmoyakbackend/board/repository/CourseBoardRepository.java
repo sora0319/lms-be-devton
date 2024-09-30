77895ebfeb2bfd0b690dd1c473a0750e3f889fa6
gomar <ju79318@naver.com>
2024-10-01 00:37:54 +0900

    mod : findByCourseAndIdAndUser,findAllByUserAndCourseOrderByCreatedAtDesc  추가

--- src/main/java/com/example/ahimmoyakbackend/board/repository/CourseBoardRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/board/repository/CourseBoardRepository.java
@@ -1,5 +1,6 @@
 package com.example.ahimmoyakbackend.board.repository;
 
+import com.example.ahimmoyakbackend.auth.entity.User;
 import com.example.ahimmoyakbackend.board.common.BoardType;
 import com.example.ahimmoyakbackend.board.entity.CourseBoard;
 import com.example.ahimmoyakbackend.course.entity.Course;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseBoardRepository extends JpaRepository<CourseBoard, Long> {
+    CourseBoard findByCourseAndIdAndUser(Course course,Long courseBoardId,User user);
     CourseBoard findByCourseAndId(Course course,Long courseBoardId);
     Page<CourseBoard> findAllByCourseAndTypeOrderByCreatedAtDesc(Course course, BoardType type, Pageable pageable);
+    Page<CourseBoard> findAllByUserAndCourseOrderByCreatedAtDesc(User user, Course course, Pageable pageable);
 }
