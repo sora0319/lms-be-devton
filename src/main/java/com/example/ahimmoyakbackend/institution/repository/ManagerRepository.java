b5001817b9ece6f71f495e17c3a0648c58ee77c1
gomar <ju79318@naver.com>
2024-10-01 00:38:48 +0900

    mod : findByInstitution 추가

--- src/main/java/com/example/ahimmoyakbackend/institution/repository/ManagerRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/institution/repository/ManagerRepository.java
@@ -1,8 +1,9 @@
 package com.example.ahimmoyakbackend.institution.repository;
 
-
+import com.example.ahimmoyakbackend.institution.entity.Institution;
 import com.example.ahimmoyakbackend.institution.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Manager findByInstitution(Institution institution);
 }
