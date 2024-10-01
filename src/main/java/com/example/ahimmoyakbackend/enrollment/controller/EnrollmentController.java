9fe99f0503c40e1b2a4bd4b53b818a831e00303d
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-10-01 17:36:09 +0900

    feat : 수강신청을 등록, 취소, 명단 넘기기 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/enrollment/controller/EnrollmentController.java
+++ src/main/java/com/example/ahimmoyakbackend/enrollment/controller/EnrollmentController.java
@@ -2,16 +2,13 @@ package com.example.ahimmoyakbackend.enrollment.controller;
 
 
 import com.example.ahimmoyakbackend.auth.config.security.UserDetailsImpl;
-import com.example.ahimmoyakbackend.enrollment.dto.EnrollmentGetListResponseDTO;
+import com.example.ahimmoyakbackend.enrollment.dto.*;
 import com.example.ahimmoyakbackend.enrollment.service.EnrollmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,8 +22,35 @@ public class EnrollmentController {
     @GetMapping("/enrollment")
     public ResponseEntity<List<EnrollmentGetListResponseDTO>> getEnrollmentList(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam Long institutionId, @RequestParam Long companyId) {
 
-        List<EnrollmentGetListResponseDTO> responseDTO = enrollmentService.getEnrollmentList(userDetails.getUser(),institutionId, companyId);
+        List<EnrollmentGetListResponseDTO> responseDTO = enrollmentService.getEnrollmentList(userDetails.getUser(), institutionId, companyId);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
     }
+
+    @PatchMapping("/registration") // 수강 신청  등록해주는거
+    public ResponseEntity<EnrollmentClassRegistrationResponseDTO> registrationEnrollment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody EnrollmentClassRegistrationRequestDTO dto) {
+
+        EnrollmentClassRegistrationResponseDTO responseDTO = enrollmentService.registration(dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+
+    }
+
+    @PatchMapping("/cancel")
+    public ResponseEntity<EnrollmentClassCancelResponseDTO> cancelEnrollment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam Long enrollmentId) {
+
+        EnrollmentClassCancelResponseDTO responseDTO = enrollmentService.cancelEnrollment(enrollmentId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @PostMapping("/employee")
+    public ResponseEntity<EnrollmentTossRosterResponseDTO> tossRoster(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody EnrollmentTossRosterRequestDTO dto) {
+
+        EnrollmentTossRosterResponseDTO responseDTO = enrollmentService.tossRoster(userDetails.getUser(), dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+
 }
