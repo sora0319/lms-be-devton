706ee792465365e11e7eab2032fab0e7291e10fe
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-10-01 17:31:15 +0900

    mod : findAllByDepartment_Company_Id 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/AffiliationRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Affiliation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AffiliationRepository extends JpaRepository<Affiliation, Long> {
+    List<Affiliation> findAllByDepartment_Company_Id(Long id);
 }
