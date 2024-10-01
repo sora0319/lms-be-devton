2fdc885d1ede40d4614e871ae7d4c48618a6f406
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-10-01 17:33:12 +0900

    mod :contract에서 courseProbide 로 변경

--- src/main/java/com/example/ahimmoyakbackend/company/repository/CourseProvideRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/CourseProvideRepository.java
@@ -6,13 +6,12 @@ import com.example.ahimmoyakbackend.course.entity.Course;
 import com.example.ahimmoyakbackend.institution.entity.Institution;
 import com.example.ahimmoyakbackend.company.entity.CourseProvide;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ContractRepository extends JpaRepository<CourseProvide, Long> {
+public interface CourseProvideRepository extends JpaRepository<CourseProvide, Long> {
     Optional<CourseProvide> findByCourseAndEnrollments_User(Course course, User user);
 
-    List<CourserProvide> findAllByCompanyAndCourse_Institution(Company company, Institution institution);
+    List<CourseProvide> findAllByCompanyAndCourse_Institution(Company company, Institution institution);
 }
