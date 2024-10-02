72213662033a29956cce6c85afd611582d5610ff
Dowon <tnstjs1919@gmail.com>
2024-10-02 23:34:13 +0900

    mod : CompanyRepository 에 findByCompanyNameContaining 추가

--- src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
@@ -4,6 +4,10 @@ import com.example.ahimmoyakbackend.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    List<Company> findByCompanyNameContaining(String companyName);
+
 }
