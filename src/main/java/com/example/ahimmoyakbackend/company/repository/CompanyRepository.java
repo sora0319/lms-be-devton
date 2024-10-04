bd748b5a74511ac5a51f0c38ccb96b6fc05a0bf3
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-10-04 16:09:35 +0900

    mod : CompanyRepository findAllById 메소드 생성

--- src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/company/repository/CompanyRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.company.repository;
 import com.example.ahimmoyakbackend.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    List<Company> findAllById(Long companyId);
 }
