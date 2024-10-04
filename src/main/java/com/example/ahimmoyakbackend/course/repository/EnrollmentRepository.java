4fe0cd3674abfb0da913c8bcb7cfe54615327de7
DdangGgoong <20191478@office.dongseo.ac.kr>
2024-10-04 16:15:36 +0900

    mod : contract에서 courseProvide로 변경

--- src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
@@ -1,13 +1,9 @@
 package com.example.ahimmoyakbackend.course.repository;
 
-import com.example.ahimmoyakbackend.company.entity.Company;
-import com.example.ahimmoyakbackend.course.entity.Course;
 import com.example.ahimmoyakbackend.course.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
-    Enrollment findByContract_Company_IdAndContract_Course_Id(Long company_id, Long course_id);
+    Enrollment findByCourseProvide_Company_IdAndCourseProvide_Course_Id(Long company_id, Long course_id);
 }
