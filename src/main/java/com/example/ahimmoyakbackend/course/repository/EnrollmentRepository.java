28f5ccbe5b5f0e9e38746a74b6dcbee58b026187
nowgnodeel <nowgnodeel123@gmail.com>
2024-10-07 16:45:31 +0900

    add : 수강신청 요청 사항 조회에 필요한 findAllByCourseProvideId 메소드 추가

--- src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
+++ src/main/java/com/example/ahimmoyakbackend/course/repository/EnrollmentRepository.java
@@ -3,5 +3,8 @@ package com.example.ahimmoyakbackend.course.repository;
 import com.example.ahimmoyakbackend.course.entity.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    List<Enrollment> findAllByCourseProvideId(Long courseProvideId);
 }
