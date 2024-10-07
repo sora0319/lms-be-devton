a0dc6987f67a8fe577d7a35da15a7105617f6a53
nowgnodeel <nowgnodeel123@gmail.com>
2024-10-07 16:43:42 +0900

    add : 수강신청 요청 사항 조회에 필요한 DTO 추가

--- src/main/java/com/example/ahimmoyakbackend/course/dto/CourseProvideListResponseDTO.java
+++ src/main/java/com/example/ahimmoyakbackend/course/dto/CourseProvideListResponseDTO.java
@@ -1,11 +1,13 @@
 package com.example.ahimmoyakbackend.course.dto;
 
+import com.example.ahimmoyakbackend.company.common.CourseProvideState;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Builder
@@ -14,5 +16,12 @@ import java.time.LocalDate;
 public class CourseProvideListResponseDTO {
     private LocalDate beginDate;
     private LocalDate endDate;
+    private CourseProvideState state;
     private int attendeeCount;
+    private String jobTitle; // 이건 뭐임
+    private String applicantName;
+    private String applicantPhone;
+    private String applicantEmail;
+    private LocalDate applicationDate;
+    private List<CourseLearnerResponseDTO> learners;
 }
