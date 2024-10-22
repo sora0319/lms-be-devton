c7efd6b99cea7bea661b6b240f282fd50574f911
Dowon <tnstjs1919@gmail.com>
2024-10-22 14:34:42 +0900

    mod : userDetails 사용으로 변경

--- src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
+++ src/main/java/com/example/ahimmoyakbackend/company/controller/CompanyController.java
@@ -5,7 +5,6 @@ import com.example.ahimmoyakbackend.auth.service.UserService;
 import com.example.ahimmoyakbackend.company.dto.*;
 import com.example.ahimmoyakbackend.company.service.CompanyService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,51 +24,59 @@ public class CompanyController {
     private final UserService userService;
 
     // EmployeeList
-    @RequestMapping(value = "/v1/supervisor/", method = RequestMethod.GET)
-    public ResponseEntity<List<CompanyInquiryEmployeeListResponseDto>> getUserList(@RequestParam("companyId") Long companyId
-    ) {
+    @GetMapping("/v1/supervisor/")
+    public ResponseEntity<List<CompanyInquiryEmployeeListResponseDto>> getUserList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long companyId = userService.getAuth(userDetails).getId();
+
         List<CompanyInquiryEmployeeListResponseDto> userList = companyService.getUserbyCompany(companyId);
         return ResponseEntity.status(HttpStatus.OK).body(userList);
-    }
 
+    }
     // EmployeeDetail
 
     // Department
-    @RequestMapping(value = "/v1/supervisor/department", method = RequestMethod.POST)
-    public ResponseEntity<CompanyEnrollDepartmentResponseDto> enrollDepartment(@RequestParam("companyId") Long companyId,
-                                                                               @RequestParam("affiliationId") Long affiliationId,
+    @PostMapping("/v1/supervisor/department")
+    public ResponseEntity<CompanyEnrollDepartmentResponseDto> enrollDepartment(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                                @RequestBody CompanyEnrollDepartmentRequestDto requestDto
     ) {
+        Long companyId = userService.getAuth(userDetails).getId();
+        Long affiliationId = userService.getAuth(userDetails).getAffiliation().getId();
+
         CompanyEnrollDepartmentResponseDto enrolled = companyService.enrollDepartment(companyId, affiliationId, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(enrolled);
     }
 
-    @RequestMapping(value = "/v1/supervisor/department", method = RequestMethod.DELETE)
-    public ResponseEntity<CompanyDeleteDepartmentResponseDto> deleteDepartment(@RequestParam("companyId") Long companyId,
-                                                                               @RequestParam("affiliationId") Long affiliationId,
-                                                                               @RequestParam("departmentId") Long departmentId
+    @DeleteMapping("/v1/supervisor/department")
+    public ResponseEntity<CompanyDeleteDepartmentResponseDto> deleteDepartment(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
-    ) {
+        Long companyId = userService.getAuth(userDetails).getId();
+        Long affiliationId = userService.getAuth(userDetails).getAffiliation().getId();
+        Long departmentId = userService.getAuth(userDetails).getAffiliation().getDepartment().getId();
+
         CompanyDeleteDepartmentResponseDto deleted = companyService.deleteDepartment(companyId, affiliationId, departmentId);
         return ResponseEntity.status(HttpStatus.OK).body(deleted);
     }
 
-    @RequestMapping(value = "v1/supervisor/department", method = RequestMethod.PATCH)
-    public ResponseEntity<CompanyUpdateDepartmentResponseDto> updateDepartment(@RequestParam("companyId") Long companyId,
-                                                                               @RequestParam("departmentId") Long departmentId,
+    @PatchMapping("/v1/supervisor/department")
+    public ResponseEntity<CompanyUpdateDepartmentResponseDto> updateDepartment(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                                @RequestBody CompanyUpdateDepartmentRequestDto requestDto
 
     ) {
+        Long companyId = userService.getAuth(userDetails).getId();
+        Long departmentId = userService.getAuth(userDetails).getAffiliation().getDepartment().getId();
+
         CompanyUpdateDepartmentResponseDto updated = companyService.updateDepartment(companyId, departmentId, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 
-    @RequestMapping(value = "v1/supervisor/department", method = RequestMethod.GET)
-    public ResponseEntity<List<CompanyInquiryDepartmentResponseDto>> inquiryDepartment(@RequestParam("companyId") Long companyId
-    ) {
+    @GetMapping("/v1/supervisor/department")
+    public ResponseEntity<List<CompanyInquiryDepartmentResponseDto>> inquiryDepartment(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long companyId = userService.getAuth(userDetails).getId();
+
         List<CompanyInquiryDepartmentResponseDto> departmentList = companyService.getDepartmentCompanyId(companyId);
         return ResponseEntity.status(HttpStatus.OK).body(departmentList);
     }
+
 
     // Company
     @PostMapping("/v1/supervisor/company")
@@ -81,11 +88,13 @@ public class CompanyController {
         return ResponseEntity.status(HttpStatus.OK).body(enrolled);
     }
 
-    @RequestMapping(value = "v1/supervisor/company", method = RequestMethod.PATCH)
-    public ResponseEntity<CompanyUpdateResponseDto> updateCompany(@RequestParam("companyId") Long companyId,
+    @PatchMapping("/v1/supervisor/company")
+    public ResponseEntity<CompanyUpdateResponseDto> updateCompany(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                   @RequestBody CompanyUpdateRequestDto requestDto
 
     ) {
+        Long companyId = userService.getAuth(userDetails).getId();
+
         CompanyUpdateResponseDto updated = companyService.updateCompany(companyId, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
