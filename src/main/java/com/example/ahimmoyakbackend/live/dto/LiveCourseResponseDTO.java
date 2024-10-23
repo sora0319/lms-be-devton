d933dda721291b408169a76502210a1782bebc2f
Miru <alfn051@gmail.com>
2024-10-23 11:42:48 +0900

    mod : live 관련 dto 에서 tutor 변수를 instructor 로 이름 변경

--- src/main/java/com/example/ahimmoyakbackend/live/dto/LiveCourseResponseDTO.java
+++ src/main/java/com/example/ahimmoyakbackend/live/dto/LiveCourseResponseDTO.java
@@ -1,7 +1,6 @@
 package com.example.ahimmoyakbackend.live.dto;
 
 import com.example.ahimmoyakbackend.company.entity.CourseProvide;
-import com.example.ahimmoyakbackend.course.entity.Course;
 import com.example.ahimmoyakbackend.live.common.LiveState;
 import com.example.ahimmoyakbackend.live.entity.LiveStreaming;
 import lombok.AllArgsConstructor;
@@ -19,7 +18,7 @@ public class LiveCourseResponseDTO {
     private Long key;
     private String title;
     private String course;
-    private String tutor;
+    private String instructor;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private LiveState state;
@@ -29,7 +28,7 @@ public class LiveCourseResponseDTO {
                 .key(liveStreaming.getId())
                 .title(liveStreaming.getTitle())
                 .course(courseProvide.getCourse().getTitle())
-                .tutor(courseProvide.getCourse().getTutor().getUser().getName())
+                .instructor(courseProvide.getCourse().getTutor().getUser().getName())
                 .startTime(liveStreaming.getStartTime())
                 .endTime(liveStreaming.getEndTime())
                 .state(liveStreaming.getState())
