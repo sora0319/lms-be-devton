39b75ad81db6aa505e2ca343476f3ed158799564
Dowon <tnstjs1919@gmail.com>
2024-10-30 18:31:11 +0900

    feat : enrollment Id 가져오는 메서드 추가

--- src/main/java/com/example/ahimmoyakbackend/enrollment/service/EnrollmentServiceImpl.java
+++ src/main/java/com/example/ahimmoyakbackend/enrollment/service/EnrollmentServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.ahimmoyakbackend.course.entity.Course;
 import com.example.ahimmoyakbackend.course.entity.Enrollment;
 import com.example.ahimmoyakbackend.course.repository.CourseRepository;
 import com.example.ahimmoyakbackend.course.repository.EnrollmentRepository;
+import com.example.ahimmoyakbackend.enrollment.dto.EnrollmentIdResponseDto;
 import com.example.ahimmoyakbackend.global.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,10 +31,10 @@ public class EnrollmentServiceImpl implements EnrollmentService {
     public boolean make(UserDetails userDetails, long courseId) {
         User user = userService.getAuth(userDetails);
         Course course = courseRepository.findById(courseId).orElse(null);
-        if(course == null) {
+        if (course == null) {
             throw new ApiException(HttpStatus.NOT_FOUND, "코스를 찾을 수 없습니다.");
         }
-        if(course.getBeginDate().isBefore(LocalDate.now())){
+        if (course.getBeginDate().isBefore(LocalDate.now())) {
             throw new ApiException(HttpStatus.METHOD_NOT_ALLOWED, "이미 시작된 코스입니다.");
         }
         enrollmentRepository.save(Enrollment.builder()
@@ -48,10 +50,10 @@ public class EnrollmentServiceImpl implements EnrollmentService {
     public boolean cancel(UserDetails userDetails, long enrollId) {
         User user = userService.getAuth(userDetails);
         Enrollment enrollment = enrollmentRepository.findById(enrollId).orElse(null);
-        if(enrollment == null) {
+        if (enrollment == null) {
             throw new ApiException(HttpStatus.NOT_FOUND, "수강신청을 찾을 수 없습니다.");
         }
-        if(enrollment.getCourse().getBeginDate().isBefore(LocalDate.now())){
+        if (enrollment.getCourse().getBeginDate().isBefore(LocalDate.now())) {
             throw new ApiException(HttpStatus.METHOD_NOT_ALLOWED, "이미 시작된 코스입니다.");
         }
         enrollmentRepository.delete(enrollment);
@@ -63,17 +65,34 @@ public class EnrollmentServiceImpl implements EnrollmentService {
     public boolean cancel(long courseId, UserDetails userDetails) {
         User user = userService.getAuth(userDetails);
         Course course = courseRepository.findById(courseId).orElse(null);
-        if(course == null) {
+        if (course == null) {
             throw new ApiException(HttpStatus.NOT_FOUND, "코스를 찾을 수 없습니다.");
         }
         Enrollment enrollment = enrollmentRepository.findByUserAndCourse(user, course).orElse(null);
-        if(enrollment == null) {
+        if (enrollment == null) {
             throw new ApiException(HttpStatus.NOT_FOUND, "수강신청을 찾을 수 없습니다.");
         }
-        if(course.getBeginDate().isBefore(LocalDate.now())){
+        if (course.getBeginDate().isBefore(LocalDate.now())) {
             throw new ApiException(HttpStatus.METHOD_NOT_ALLOWED, "이미 시작된 코스입니다.");
         }
         enrollmentRepository.delete(enrollment);
         return true;
+    }
+
+    @Override
+    @Transactional
+    public EnrollmentIdResponseDto getEnrollId(UserDetails userDetails, Long courseId) {
+        User user = userService.getAuth(userDetails);
+
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("코스정보가 존재하지 않습니다 "));
+
+        Optional<Enrollment> enrollmentOptional = enrollmentRepository.findByUserIdAndCourseId(user.getId(), course.getId());
+
+        Long enrollmentId = enrollmentOptional.map(Enrollment::getId).orElse(null);
+
+        return EnrollmentIdResponseDto.builder()
+                .id(enrollmentId)
+                .build();
     }
 }
