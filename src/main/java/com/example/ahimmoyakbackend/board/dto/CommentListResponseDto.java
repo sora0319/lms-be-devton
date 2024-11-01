c8e436b63a07a6cf126ab2254ed34ee7f4185792
gomar <ju79318@naver.com>
2024-11-01 10:29:03 +0900

    mod : CommentListResponseDto에 commentId가 필요해서 추가

--- src/main/java/com/example/ahimmoyakbackend/board/dto/CommentListResponseDto.java
+++ src/main/java/com/example/ahimmoyakbackend/board/dto/CommentListResponseDto.java
@@ -6,12 +6,14 @@ import lombok.Builder;
 @Builder
 public record CommentListResponseDto(
     String content,
-    String user
+    String user,
+    Long commentId
 ) {
     public static CommentListResponseDto from(CourseComment comment) {
         return CommentListResponseDto.builder()
                 .content(comment.getContent())
                 .user(comment.getUser().getUsername())
+                .commentId(comment.getId())
                 .build();
     }
 }
